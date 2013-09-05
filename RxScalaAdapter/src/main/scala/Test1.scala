4fe60f26aadf2b2015fc8fa73b3e9dc0ad9413fa
samuelgruetter <samuel.gruetter@epfl.ch>
2013-09-05 17:57:00 +0200

    question on Function subtyping

--- RxScalaAdapter/src/main/scala/Test1.scala
+++ RxScalaAdapter/src/main/scala/Test1.scala
@@ -1,10 +1,12 @@
 
-import rx.lang.scala._
+import rx.lang.scala.Adaptor._ // TODO replace with new one
+import rx.util.functions.Func1
+import rx.util.functions.Action1
 
 object Test1 {
 
   def printInt(x: Int) = println(x)
-  
+
   def main(args: Array[String]): Unit = {
     val o1 = Observable(1, 2, 3, 4)
     o1.subscribe((x: Int) => println(x))
@@ -19,6 +21,20 @@ object Test1 {
     
     o1.drop(2).subscribe(printInt(_), (t: Throwable) => {}, () => println("complete"))
     
+  }
+  
+  // Question: Is it a problem that in rx.Observable, some functions take an Action1<Throwable>
+  // instead of an Action1<? super Throwable> ?
+  // Answer: No problem. Look at this example:
+  def foo() = {
+    val myFunc: Any => Unit = (o: Any) => println(o)
+    
+    val f: Action1[Throwable] = new Action1[Throwable]() {
+      // this is what the implicit conversion do:
+      def call(t: Throwable) {
+        myFunc(t) // no one cares that myFunc accepts more than only Throwable
+      }
+    }
   }
 
 }
