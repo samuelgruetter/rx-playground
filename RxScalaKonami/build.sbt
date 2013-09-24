da1988a367faf35788f06694844b21b56b23baa7
samuelgruetter <samuel.gruetter@epfl.ch>
2013-09-24 15:11:23 +0200

    konamicode example

---
+++ RxScalaKonami/build.sbt
@@ -0,0 +1,15 @@
+name := "RxScalaKonami"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+val rxVersion = "0.14.1"
+
+libraryDependencies ++= Seq(
+	"com.netflix.rxjava" % "rxjava-core"  % rxVersion ,
+	"com.netflix.rxjava" % "rxjava-swing" % rxVersion ,
+	"com.netflix.rxjava" % "rxjava-scala" % rxVersion
+)
+
+
---
+++ RxScalaKonami/src/main/scala/Konami.scala
@@ -0,0 +1,57 @@
+
+import java.awt.BorderLayout
+import java.awt.event.KeyEvent
+
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.SwingConstants
+import rx.lang.scala.Observable
+import rx.observables.SwingObservable
+
+
+class Win1 extends JFrame {
+  val label = new JLabel("")
+  
+  def run = {
+    initLayout
+    
+    val konami = Observable(
+        38, // up
+        38, // up
+        40, // down
+        40, // down
+        37, // left
+        39, // right
+        37, // left
+        39, // right
+        66, // b
+        65  // a
+    )
+    
+    val pressedKeys = Observable(SwingObservable.fromKeyEvents(this))
+                        .filter(_.getID() == KeyEvent.KEY_RELEASED)
+                        .map(_.getKeyCode())
+    
+    val bingo = pressedKeys.window(10, 1)
+                  .flatMap(window => (window zip konami).forall(p => p._1 == p._2))
+                  .filter(b => b)
+    
+    bingo.subscribe(_ => label.setText(label.getText() + " KONAMI "))
+  }
+   
+  def initLayout = {
+    setLayout(new BorderLayout)
+    label.setHorizontalAlignment(SwingConstants.CENTER)
+    add(label, BorderLayout.CENTER)
+    setSize(400, 400)
+    setResizable(false)
+    setTitle("Enter the Konami Code!")
+	setVisible(true)
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  }  
+}
+
+object Konami extends App {  
+  new Win1().run
+}
+
